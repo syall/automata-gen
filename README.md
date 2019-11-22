ff7d9db4b4e0a1a7bc4ceb9ece681c6e6187d027
syall <s.yuan.all@gmail.com>
2019-11-22 17:41:55 -0500

    update to typedoc

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# gol
+# Cellular Automata Generator
+
+## The "What" of this Package
+
+* Given a Rules object, one can specify how to generate a cellular automata
+
+* Flexiblity in state as well as displaying characters
+
+* Provides an easy framework to print and update the Cellular Automata, including timing, size of the grid, and maximum iterations
+
+## Motivation
+
+Personally, Cellular Automata are a fascinating subject!
+
+Although there are a lot of academic papers which discuss the various applications of this simple concept, there was no simple way to generalize creating cellular automata.
+
+Although this may not be the most optimal solution, it supports a variety of options that seem helpful instead of having to code all of the special cases.
+
+## Installation
+
+To use this package using npm:
+
+~~~shell
+npm install automata-gen
+~~~
+
+or using yarn:
+
+~~~shell
+yarn add automata-gen
+~~~
+
+## Personal Note
+
+Cellular Automata generator is the first package I have ever dared to publish.
+
+The culmination of my learning and exposure to the programming community is this package.
+
+Many beginner programmers have coded Conway's game of life, and I hope this contribution can help those understand the power of these simple simulations.
+
+Thank you,
+
+syall
--- src/automata.ts
+++ src/automata.ts
@@ -1,17 +1,50 @@
 /**
- * Game of Life
+ * Cellular Automata Generator
  */
 
-const rows = process.argv[3] || 30;
-const cols = process.argv[4] || 30;
-const stepMs = 100;
-const maxIterations = 500;
-let iteration = 0;
+/**
+ * Representing State and Character of a Cell in the [[Grid]]
+ */
+interface Cell {
+	/** State associated with the Cell */
+	state: String | number;
+	/** Character to display when [[printGrid]] is used */
+	display: String;
+}
 
-function rulesParser(path) {
+/**
+ * Universe of the Cellular Automata
+ */
+type Grid = Cell[][];
+
+interface InitCell {
+	cell: Cell;
+	probability: number;
+}
+
+interface Options {
+	random?: Boolean;
+	range?: InitCell[];
+}
+
+interface GridState {
+	grid: Grid;
+	change: boolean;
+}
+
+// TODO: Find type for Rules Parser
+
+const rows: number = Number.parseInt(process.argv[3]) || 30;
+const cols: number = Number.parseInt(process.argv[4]) || 30;
+const stepMs: number = 100;
+const maxIterations: number = 500;
+let iteration: number = 0;
+
+// TODO: Figure out rules format
+function rulesParser(path: string) {
 	const fs = require('fs');
-	const content = fs.readFileSync(path).toString();
-	const rmws = r => r !== '';
+	const content: String = fs.readFileSync(path).toString();
+	const rmws = (r: String): Boolean => r !== '';
 	const stmts = content
 		.split('\n')
 		.filter(rmws)
@@ -45,51 +78,56 @@ function rulesParser(path) {
 	return parsed;
 }
 
-function generateGrid(options = {}) {
-	const opts = {
+function generateGrid(options?: Options): Grid {
+	const opts: Options = {
 		random: false,
-		low: 0,
-		high: 1,
+		range: [
+			{ cell: { state: 0, display: '0' }, probability: 0.7 },
+			{ cell: { state: 1, display: '1' }, probability: 0.3 },
+		],
 		...options,
 	};
-	const grid = [];
+	const grid: Grid = [];
 	for (let row = 0; row < rows; row++) {
 		grid.push([]);
-		for (let col = 0; col < cols; col++)
-			grid[row].push(opts.random
-				? randomInRange(opts.low, opts.high)
-				: opts.low
-			);
+		for (let col = 0; col < cols; col++) {
+			// TODO: Update logic to include Cell
+			// grid[row].push(opts.random
+			// 	? randomInRange(opts.low, opts.high)
+			// 	: opts.low
+			// );
+		}
 	}
 	return grid;
 }
 
+// TODO: Differentiate Random based on InitCell
 function randomInRange(low, high) {
 	return Math.floor(Math.random() * (high - low + 1)) + low;
 }
 
-function printGrid(grid) {
+function printGrid(grid: Grid): void {
 	console.clear();
 	console.log(`Iteration: ${iteration++}`);
 	console.log(grid.map(row => row.join('')).join('\n'));
 }
 
-function updateGrid(grid, rules) {
-	const tempGrid = generateGrid();
-	let change = false;
+function updateGrid(grid: Grid, rules): GridState {
+	const tempGrid: Grid = generateGrid();
+	let change: boolean = false;
 	for (let row = 0; row < rows; row++) {
 		for (let col = 0; col < cols; col++) {
-			const numNeighbors = getNeighbors(grid, row, col);
-			const status = checkRules(rules, numNeighbors, grid[row][col]);
+			const numNeighbors: number = getNeighbors(grid, row, col);
+			const status: Cell = checkRules(rules, numNeighbors, grid[row][col]);
 			tempGrid[row][col] = status;
 			if (tempGrid[row][col] !== grid[row][col])
 				change = true;
 		}
 	}
-	return { mainGrid: tempGrid, change };
+	return { grid: tempGrid, change };
 }
 
-function getNeighbors(grid, r, c) {
+function getNeighbors(grid: Grid, r, c) {
 	let count = 0;
 	const directions = [
 		[+0, +1],
@@ -105,38 +143,41 @@ function getNeighbors(grid, r, c) {
 		const newR = r + offX;
 		const newC = c + offY;
 		if (newR >= 0 && newR < rows && newC >= 0 && newC < cols) {
-			count = grid[newR][newC] !== 0
-				? count + 1
-				: count;
+			// TODO: Update Logic for Cell
+			// count = grid[newR][newC] !== 0
+			// 	? count + 1
+			// 	: count;
 		}
 	});
 	return count;
 }
 
-function checkRules(rules, n, prev) {
-	let status;
+// TODO: Update CheckRules after figuring out rules format
+function checkRules(rules, n: number, prev: Cell) {
+	let status: Cell;
 	for (let i = 0; i < rules.length; i++) {
 		status = rules[i](n);
-		if (status !== false)
-			return status.res;
+		// if (status !== false)
+		// 	return status.res;
 	}
 	return prev;
 }
 
-function sleep(milliseconds) {
+/** @ignore */
+function sleep(milliseconds: number): Promise<void> {
 	return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
-async function main() {
+async function main(): Promise<void> {
 	console.log('Loading...');
 	const rules = process.argv[2]
 		? rulesParser(`rules/${process.argv[2]}`)
 		: rulesParser('rules/default.txt');
-	let mainGrid = generateGrid({ random: true });
-	let running = true;
+	let mainGrid: Grid = generateGrid({ random: true });
+	let running: boolean = true;
 	while (running) {
 		printGrid(mainGrid);
-		const { mainGrid: tempGrid, change } = updateGrid(mainGrid, rules);
+		const { grid: tempGrid, change }: GridState = updateGrid(mainGrid, rules);
 		mainGrid = tempGrid;
 		running = change && iteration < maxIterations;
 		await sleep(stepMs);
